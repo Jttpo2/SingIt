21ba149471a22e20c3c06fdf2ac363f882d3c2e6
John Petersson <johnpetersson@gmail.com>
2015-05-20 20:01:57 +0200

    lyric class added for reals

---
+++ app/src/main/java/com/theasumlab/singit/model/Lyric.java
@@ -0,0 +1,71 @@
+package com.theasumlab.singit.model;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by johnpetersson on 2015-05-20.
+ */
+public class Lyric {
+//    private String content;
+    private static final String TEST_LYRIC = "Pulled into nazareth/nWas Feeling 'bout half past dead/nI just needed a place where/nI could lay my head";
+
+    private List<String> content;
+    private ListIterator<String> iterator;
+
+    public Lyric() {
+        this(TEST_LYRIC);
+    }
+
+    public Lyric(String content) {
+        this.content = new LinkedList<String>();
+        iterator = this.content.listIterator();
+        addToContent(content);
+
+    }
+
+    public String next() {
+        return iterator.next();
+    }
+
+//    look ahead (or back) without moving iterator
+    public String peek(int amount) {
+        int currentIndex = 0;
+        if (iterator.hasNext()) {
+            currentIndex = iterator.nextIndex() -1;
+        }
+        int targetIndex = currentIndex + amount;
+
+        if (targetIndex >= content.size() && targetIndex >= 0 ) {
+            // keep inside the bounds of the list
+            return content.get(targetIndex);
+        }
+
+        return null;
+    }
+
+
+
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    public String previous() {
+        return iterator.previous();
+    }
+
+    public boolean hasPrevious() {
+        return iterator.hasPrevious();
+    }
+
+    private void addToContent(String text) {
+        String splitter = "/n";
+        String[] lines = text.split(splitter);
+        for (String str: lines) {
+            iterator.add(str);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/theasumlab/singit/model/Singit.java
@@ -0,0 +1,14 @@
+package com.theasumlab.singit.model;
+
+/**
+ * Created by johnpetersson on 2015-05-20.
+ */
+public class Singit {
+    private Lyric activeLyric;
+
+    public Singit() {
+        activeLyric = new Lyric();
+    }
+
+}
+
